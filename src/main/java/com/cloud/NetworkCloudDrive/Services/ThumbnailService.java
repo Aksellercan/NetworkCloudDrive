088cb536f00b83884923fa48f24689e4a43a1ccc
Aksell <akselercan2003@gmail.com>
2026-01-22 16:34:04 +0100

    initial

--- src/main/java/com/cloud/NetworkCloudDrive/Services/ThumbnailService.java
+++ src/main/java/com/cloud/NetworkCloudDrive/Services/ThumbnailService.java
@@ -1,4 +1,14 @@
 package com.cloud.NetworkCloudDrive.Services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ThumbnailService {
+    private final Logger logger = LoggerFactory.getLogger(ThumbnailService.class);
+
+    public ThumbnailService() {
+
+    }
+
+
 }
