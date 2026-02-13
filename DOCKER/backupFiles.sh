6a9e11695270992aaa967bbc24995cd727c4d787
Aksell <akselercan2003@gmail.com>
2026-02-13 16:24:23 +0100

    fix: scripts

--- DOCKER/backupFiles.sh
+++ DOCKER/backupFiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Starting Docker backup script"
+echo "Starting Docker Backup script"
 container_id=$(docker ps -aqf "name=NetworkCloudDriveDocker")
 echo "Found CONTAINER ID: ${container_id}"
 # ENTER CONTAINER AND RETRIEVE /app FOLDER
@@ -7,13 +7,14 @@ docker cp $container_id:app/ .
 echo "Copy complete"
 echo "Now extracting root folder, db and config folder"
 time_stamp=$(date +%Y-%m-%d-%T)
-mkdir ./$time_stamp
-echo "Created folder ${time_stamp}"
-mv ./app/filedatabase.db ./$time_stamp
+backup_folder="NetworkCloudDriveDocker_$time_stamp"
+mkdir ./$backup_folder
+echo "Created folder ${backup_folder}"
+mv ./app/filedatabase.db ./$backup_folder
 echo "Moved ./app/filedatabase.db"
-mv ./app/root ./$time_stamp
+mv ./app/root ./$backup_folder
 echo "Moved ./app/root/"
-mv ./app/config ./$time_stamp
+mv ./app/config ./$backup_folder
 echo "Moved ./app/config/"
 rm -r ./app
 echo "Cleaned up"
--- DOCKER/restoreFiles.sh
+++ DOCKER/restoreFiles.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-echo "Starting Docker restore script"
+echo "Starting Docker Restore script"
 container_id=$(docker ps -aqf "name=NetworkCloudDriveDocker")
 echo "Found CONTAINER ID: ${container_id}"
 echo "Stopping container ${container_id}"
 docker stop $container_id
 echo "Stopped container"
-docker cp ./root $container_id:app/
-echo "Copied ./root folder to ${container_id}:app/"
-docker cp ./filedatabase.db $container_id:app/
-echo "Copied ./filedatabase.db to ${container_id}:app/"
-docker cp ./config $container_id:app/
-echo "Copied ./config to ${container_id}:app/"
+echo "Looking for source folder"
+source=$(ls -d NetworkCloudDriveDocker_* | sort | tail -n 1)
+echo "Source folder: ${source}"
+docker cp ./$source/root $container_id:app/
+echo "Copied ./$source/root folder to ${container_id}:app/"
+docker cp ./$source/filedatabase.db $container_id:app/
+echo "Copied ./$source/filedatabase.db to ${container_id}:app/"
+docker cp ./$source/config $container_id:app/
+echo "Copied ./$source/config to ${container_id}:app/"
 docker start $container_id
 echo "Started container ${container_id}"
 echo "Finished restoring files to Docker Container"
