0f2f9adba1395f2417e65eb5a058f3a89c8c5118
Aksell <akselercan2003@gmail.com>
2026-04-21 11:42:27 +0200

    write script in python

--- scripts/mvn.sh
+++ scripts/mvn.sh
@@ -1,6 +1,35 @@
 #!/bin/sh
-if [ $# = 1 ]; then
-    mvn -DnewVersion="$1" versions:set
+if [ $# = 2]; then
+    if [ $1 == "-m" ]; then
+        increment_minor
+    elif [ $1 == "-M" ]; then
+        increment_major
+    elif [ $1 == "-r" ]; then
+        release_version
+    elif [ $1 == "-s" ]; then
+        snapshot_version
+    fi
 else
-    echo "invalid. usage [VERSION]"
+    echo "Invalid usage. USAGE [OPTION] [ARGUMENT]"
 fi
+
+increment_minor() {
+    local cut=$(echo "$2" | cut -d "." -f 3)
+    local new_version=$()
+}
+
+increment_major() {
+    local new_version = ''
+}
+
+release_version() {
+    local new_version = ''
+}
+
+snapshot_version() {
+    local new_version = ''
+}
+
+update_pom() {
+    mvn -DnewVersion="$1" versions:set
+}
