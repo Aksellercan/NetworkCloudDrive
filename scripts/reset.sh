f5bc60c6423dfade32ed3cb66fa2c4b12c72ea5e
Aksell <akselercan2003@gmail.com>
2026-04-29 19:52:49 +0200

    fix bash typo

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,8 +1,6 @@
-#/bin/bash
+#! /bin/bash
 rm -rf ./root #remove entire folder and contents
 echo "removed root folder"
 
 rm filedatabase.db
 echo "removed db file"
-
-#can combine rm command into single line: rm -rf ./root filedatabase.db
